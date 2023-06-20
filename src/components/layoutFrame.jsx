460b28bb32853c5098428e40ea583672b2937233
NightDevilPT <pawankumartadagsingh@gmail.com>
2023-06-20 17:50:37 +0530

    updating 2

--- src/components/layoutFrame.jsx
+++ src/components/layoutFrame.jsx
@@ -1,9 +1,33 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from "react";
+
+import { BsArrowUpCircleFill } from "react-icons/bs";
 
 const LayoutFrame = ({ children }) => {
-  return (
-    <div className='App'>{children}</div>
-  )
-}
+  const scrollRef = useRef();
+  const [hideScrollBtn, setHideScrollBtn] = useState(true);
 
-export default LayoutFrame
+  const GoScrollTop = (e) => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollTo({
+        top:"0",
+        behavior:"smooth"
+      })
+    }
+  }
+
+  return (
+    <div className="App" ref={scrollRef} onScroll={e => {
+      // console.log(e.target.scrollTop)
+      if (e.target.scrollTop > 80) {
+        setHideScrollBtn(false);
+      } else {
+        setHideScrollBtn(true);
+      }
+    }}>
+      {children}
+      <BsArrowUpCircleFill onClick={e=>{GoScrollTop(e)}} style={{display:hideScrollBtn?'none':"block"}} className="scroll-top-icon" />
+    </div>
+  );
+};
+
+export default LayoutFrame;
